633d6a0b3307dd782b1e9306726a3091e2b2adc6
Alexander Kastil <alexander.kastil@integrations.at>
2025-08-11 19:28:02 +0200

    Add student data CSV and introductory documentation
    - Created a new CSV file containing student information including IDs, names, schools, and ages.
    - Added a README file for the Semantic Kernel introduction.
    - Updated the main README to include a link for developing AI solutions in Azure.

---
+++ demos/02-agents/00-sk-intro/01-sk-chat-completion/sk-chat-completion/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration configuration = builder.Build();
+
+var model = configuration["DeploymentModel"];
+var endpoint = configuration["Endpoint"];
+var key = configuration["ApiKey"];
+
+if (string.IsNullOrWhiteSpace(model))
+    throw new ArgumentNullException(nameof(model), "DeploymentModel is missing from configuration.");
+if (string.IsNullOrWhiteSpace(endpoint))
+    throw new ArgumentNullException(nameof(endpoint), "Endpoint is missing from configuration.");
+if (string.IsNullOrWhiteSpace(key))
+    throw new ArgumentNullException(nameof(key), "ApiKey is missing from configuration.");
+
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+kernelBuilder.AddAzureOpenAIChatCompletion(
+    deploymentName: model,
+    apiKey: key,
+    endpoint: endpoint
+);
+Kernel kernel = kernelBuilder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory history = new ChatHistory();
+history.AddSystemMessage("You are a helpful poetic assistant.");
+history.AddUserMessage("Tell me a short poem about whippets and the sea.");
+
+var response = chatCompletionService.GetStreamingChatMessageContentsAsync(
+    history,
+    kernel: kernel
+);
+
+await foreach (var chunk in response)
+{
+    Console.Write(chunk);
+}
+
+Console.WriteLine("\nNow let's try an image to demonstrate multimodal capabilities.");
+byte[] bytes = File.ReadAllBytes("data/soi-beach.jpg");
+
+history.AddUserMessage([
+    new TextContent("What is this image about?"),
+    new ImageContent(bytes, "image.jpg")
+]);
+
+var reply = await chatCompletionService.GetChatMessageContentAsync(history);
+Console.WriteLine(reply.Content);
---
+++ demos/readme.md
@@ -0,0 +1,3 @@
+# Develop AI solutions in Azure
+
+[Develop AI solutions in Azure](https://learn.microsoft.com/en-us/training/courses/ai-102t00)
